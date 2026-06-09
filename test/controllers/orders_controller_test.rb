49daad1fd844a3e2c9e6134a79e77162a3020843
Rom BALAX <rbalax@yahoo.co.uk>
2026-06-09 11:40:16 +0900

    I18n done

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -23,7 +23,7 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
     end
 
     # assert_redirected_to order_url(Order.last)
-    assert_redirected_to store_index_url
+    assert_redirected_to store_index_url(locale: "en")
   end
 
   test "should show order" do
